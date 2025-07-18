cd6e0550c95c751cf20fa22fa1f4e85880f93391
Deztini <agbatordestiny26@gmail.com>
2025-07-18 10:09:54 +0100

    added environmental variables

--- app.js
+++ app.js
@@ -3,17 +3,18 @@ const mongoose = require("mongoose");
 const cors = require("cors");
 const authRoutes = require("./routes/auth.js");
 
+require("dotenv").config();
+
 const app = express();
 app.use(cors());
 app.use(express.json());
 app.use("/auth", authRoutes);
 
-const PORT = 3000;
+const PORT = process.env.PORT;
+const DB_URL = process.env.DB_URL;
 
 mongoose
-  .connect(
-    "mongodb+srv://Destiny:clubworldcup@cluster0.hqlinuf.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
-  )
+  .connect(`${DB_URL}`)
   .then(() => {
     console.log("Connected to MongoDB Atlas!");
     app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
